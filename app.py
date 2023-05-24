f79dd8e9f3202148ab3aea83d05e263098d0c59d
STC1702 <73183412+congltk1234@users.noreply.github.com>
2023-05-24 08:39:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def main():
     
     page_selection = st.sidebar.radio("Try", page_options)
 
-    articles_df = pd.read_csv('articles.csv')
+#     articles_df = pd.read_csv('articles.csv')
     
     models = ['Similar items based on image embeddings', 
               'Similar items based on text embeddings', 
